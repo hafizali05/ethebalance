e8db1f4e30a0ae066c7a820b39eb180b457f4e43
Hafiz <hafizdidarali@gmail.com>
2017-03-12 10:10:00 +0000

    Code cleanup

--- server.js
+++ server.js
@@ -23,24 +23,8 @@ app.post('/', async (req, res)=> {
 	console.log('working',blockcypher.data);
 	const bal =  blockcypher.data.balance /Math.pow(10, 18);
 	console.log('bal:',bal);
-
 	return res.render('index', { message: bal });
-	// axios.get(`https://api.blockcypher.com/v1/eth/main/addrs/${req.body.address}/balance`)
-	//   .then(function(response){
-	//     console.log(response.data); // ex.: { user: 'Your User'}
-	//     console.log(response.status); // ex.: 200
-	// 	res.render('index', { message: response.data.balance });
-	//   }); 
 });
- 
-// app.post('/', wrap(async (req, res, next) => {
-//   let wei = await bal(response.data.balance)
-//   let ether = converter(wei){
-//   	return wei / 10^8
-//   }
-//   stream.on('error', next).pipe(ether)
-// }))
-
 
 app.listen(port, () => {
   console.log('Server is up on port 3000');
